a15b03782d3d6bd14e96aa5aa078cc564b3b1e53
Francesco Pizzino <ciccio@MacBook-Pro-di-Francesco.local>
2024-02-27 15:10:59 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+class Application {
+
+// Dati
+
+ 
+ users = [];
+ username = ""
+ 
+
+ // Azioni
+ onLogin (username) {
+    this.username = username;
+ }
+
+ onLogout () {
+this.username = ""
+ }
+
+
+}
+
+
+window.onload = () => {
+    // Verifica se l'utente è già loggato
+    const storedUsername = localStorage.getItem("username");
+    if (storedUsername) {
+      app.onLogin(storedUsername);
+    
